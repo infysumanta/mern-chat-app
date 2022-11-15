d598e267a17fc9247519457351f635a4f3f062ff
Sumanta Kabiraj <sumantablog@gmail.com>
2022-11-15 14:39:17 +0530

    login and sign up ui

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,54 @@
 import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabPanels,
+  TabPanel,
+  TabList,
+  Tab,
+} from "@chakra-ui/react";
+
+import Login from "./../components/authentication/Login";
+import Signup from "./../components/authentication/Signup";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        textAlign="center"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans">
+          Chat App
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
