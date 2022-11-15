b1c70daca93685f6846f4c833684749059d0c3e6
Sumanta Kabiraj <sumantablog@gmail.com>
2022-11-15 13:35:53 +0530

    axios and fetch with proxy for cors error

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage";
+import { Route } from "react-router-dom";
+import ChatPage from "./pages/ChatPage";
 function App() {
   return (
     <div className="App">
-      <Button colorScheme="blue">Button</Button>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/chats" component={ChatPage} />
     </div>
   );
 }
