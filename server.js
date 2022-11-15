ab4a2c656d8971a1f04d5580cd14972c47c956f0
Sumanta Kabiraj <sumantablog@gmail.com>
2022-11-15 13:23:10 +0530

    basic server config

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const chats = require("./data/data");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Api is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, console.log(`Server is Started on POST ${PORT}`));
